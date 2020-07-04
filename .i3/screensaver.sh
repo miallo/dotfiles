810620aae7a08f8e4f9bc4f0e64d88959d255683
Michael Lohmann <michael@debian.debian-miallo.de>
2020-07-04 13:36:12 +0200

    Linux config and renamed to .name

---
+++ .i3/screensaver.sh
@@ -0,0 +1,20 @@
+#!/bin/sh -e
+
+# Take a screenshot
+import -silent -window root ~/.screen_locked.png
+
+# Pixellate it 10x
+convert ~/.screen_locked.png -blur 0x5 -brightness-contrast -5x-30 -gravity Center -pointsize 50 -fill yellow -draw 'text 0,0 "Dieser PC wird gerade benutzt!"' ~/.screen_locked.png
+#convert ~/.screen_locked.png -blur 0x5 -contrast-stretch 0.55 -gravity Center -pointsize 50 -fill yellow -draw 'text 0,0 "Dieser PC wird gerade benutzt!"' ~/.screen_locked.png
+
+# Lock screen displaying this image.
+i3lock -i ~/.screen_locked.png
+
+# Turn the screen off after a delay.
+for i in $(seq 1 20)
+do
+    echo $i
+	sleep 1
+	pgrep i3lock > /dev/null || exit 0
+done
+pgrep i3lock > /dev/null && xset dpms force off
---
+++ instructions.md
@@ -0,0 +1,7 @@
+# i3
+
+`apt install i3 i3status dmenu xterm`
+
+`cp Xdefaults ~/.Xdefaults && xrdb ~/.Xdefaults`
+
+
