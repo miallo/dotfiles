b411a27fcd3dc1ff983a9acb3ee80f627ca15656
Michael Lohmann <michael.lohmann@bevuta.com>
2020-11-13 23:17:28 +0100

    Use specific neovim commit

--- nixos/overlays/neovim.nix
+++ nixos/overlays/neovim.nix
@@ -1,14 +1,14 @@
 self: super: {
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (oldAttrs: {
-    version = "master";
-    # src = builtins.fetchTarball {
-      # url = https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz;
-    src = builtins.fetchGit {
-      url = https://github.com/neovim/neovim.git;
-    #   owner = "neovim";
-    #   repo = "neovim";
-    #   rev = "nightly";
-    #   fetchSubmodules = true;
+    version = "2020-11-11";
+    # src = builtins.fetchGit {
+    #   url = https://github.com/neovim/neovim.git;
+    # };
+    src = super.fetchFromGitHub {
+      owner = "neovim";
+      repo = "neovim";
+      rev = "0a95549d66df63c06d775fcc329f7b63cbb46b2f";
+      sha256 = "0a54xjd31dvz32hkzc8qhjnnp6g7v3npk5a5bdnlzh2mhxjnqyl4";
     };
     patches = [ ./system_rplugin_manifest.patch ];
     buildInputs = oldAttrs.buildInputs ++ [ super.pkgs.tree-sitter ];
