d9840ce6d3ea1d7502efc597e61fda0ad0a288d2
Michael Lohmann <michael.lohmann@bevuta.com>
2020-11-13 19:50:52 +0100

    Move vimrc to seperate file

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -184,9 +184,8 @@ in {
   #};
 
   # Enable the OpenSSH daemon.
-  # services.openssh.enable = true;
+  services.openssh.enable = true;
   programs.ssh.startAgent = true;
-  services.sshd.enable = true;
 
   # Open ports in the firewall.
   # networking.firewall.allowedTCPPorts = [ ... ];
--- nixos/overlays/neovim.nix
+++ nixos/overlays/neovim.nix
@@ -1,8 +1,12 @@
 self: super: {
   neovim-unwrapped = super.neovim-unwrapped.overrideAttrs (oldAttrs: {
     version = "master";
-    src = builtins.fetchGit {
+    # src = builtins.fetchTarball {
+    #   url = https://github.com/neovim/neovim/releases/download/nightly/nvim-linux64.tar.gz;
+    src = builtins.fetchFromGithub {
       url = https://github.com/neovim/neovim.git;
+      rev = "nightly";
+      fetchSubmodules = true;
     };
   });
 }
