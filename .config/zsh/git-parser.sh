c142f5f22c05226b2ca2f3e2f44f830a39baa6f7
Michael Lohmann <michael.lohmann@bevuta.com>
2020-12-20 00:44:43 +0100

    git-prompt: Make variables replaceable

--- .config/zsh/git-parser.sh
+++ .config/zsh/git-parser.sh
@@ -4,20 +4,20 @@
 
 autoload -U colors && colors
 
-ZSH_THEME_GIT_PROMPT_PREFIX="("
-ZSH_THEME_GIT_PROMPT_SUFFIX=")"
-ZSH_THEME_GIT_PROMPT_SEPARATOR="|"
-ZSH_THEME_GIT_PROMPT_BRANCH="%{$fg_bold[magenta]%}"
-ZSH_THEME_GIT_PROMPT_STAGED="%{$fg[green]%}%{●%G%}"
-ZSH_THEME_GIT_PROMPT_CONFLICTS="%{$fg[red]%}%{✖%G%}"
-ZSH_THEME_GIT_PROMPT_RENAMED_OR_MOVED="%{$fg[cyan]%}%{·%G%}"
-ZSH_THEME_GIT_PROMPT_DELETED="%{$fg[red]%}%{-%G%}"
-ZSH_THEME_GIT_PROMPT_CHANGED="%{$fg[blue]%}%{✚%G%}"
-ZSH_THEME_GIT_PROMPT_ADDED="%{$fg[yellow]%}%{+%G%}"
-ZSH_THEME_GIT_PROMPT_BEHIND="%{↓%G%}"
-ZSH_THEME_GIT_PROMPT_AHEAD="%{↑%G%}"
-ZSH_THEME_GIT_PROMPT_UNTRACKED="%{…%G%}"
-ZSH_THEME_GIT_PROMPT_CLEAN="%{$fg_bold[green]%}%{✔%G%}"
+: "${ZSH_THEME_GIT_PROMPT_PREFIX:="("}"
+: "${ZSH_THEME_GIT_PROMPT_SUFFIX:=")"}"
+: "${ZSH_THEME_GIT_PROMPT_SEPARATOR:="|"}"
+: "${ZSH_THEME_GIT_PROMPT_BRANCH:="%{$fg_bold[magenta]%}"}"
+: "${ZSH_THEME_GIT_PROMPT_STAGED:="%{$fg[green]%}%{●%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_CONFLICTS:="%{$fg[red]%}%{✖%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_RENAMED_OR_MOVED:="%{$fg[cyan]%}%{·%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_DELETED:="%{$fg[red]%}%{-%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_CHANGED:="%{$fg[blue]%}%{✚%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_ADDED:="%{$fg[yellow]%}%{+%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_BEHIND:="%{↓%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_AHEAD:="%{↑%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_UNTRACKED:="%{…%G%}"}"
+: "${ZSH_THEME_GIT_PROMPT_CLEAN:="%{$fg_bold[green]%}%{✔%G%}"}"
 
 get_tagname_or_hash() {
     # get hash
