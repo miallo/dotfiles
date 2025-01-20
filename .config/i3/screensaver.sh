982716acbeb014fc28eb8c9821ca459e3da2c262
Michael Lohmann <michael.lohmann@bevuta.com>
2025-01-20 10:54:06 +0100

    i3/screensaver: static lock file

--- .config/i3/screensaver.sh
+++ .config/i3/screensaver.sh
@@ -1,14 +1,15 @@
 #!/bin/sh -e
+i3lock -i ~/.config/i3/lock.png
 
-# Take a screenshot
-import -silent -window root /tmp/.screen_locked.png
-
-# Pixellate it 10x
-convert /tmp/.screen_locked.png -blur 0x5 -brightness-contrast -5x-30 -gravity Center -pointsize 50 -font DejaVu-Sans -fill yellow -draw 'text 0,0 "Dieser PC wird gerade benutzt!"' /tmp/.screen_locked.png
-#convert /tmp/.screen_locked.png -blur 0x5 -contrast-stretch 0.55 -gravity Center -pointsize 50 -fill yellow -draw 'text 0,0 "Dieser PC wird gerade benutzt!"' /tmp/.screen_locked.png
-
-# Lock screen displaying this image.
-i3lock -i /tmp/.screen_locked.png
+#  # Take a screenshot
+#  import -silent -window root /tmp/.screen_locked.png
+#  
+#  # Pixellate it 10x
+#  convert /tmp/.screen_locked.png -blur 0x5 -brightness-contrast -5x-30 -gravity Center -pointsize 50 -font DejaVu-Sans -fill yellow -draw 'text 0,0 "Dieser PC wird gerade benutzt!"' /tmp/.screen_locked.png
+#  #convert /tmp/.screen_locked.png -blur 0x5 -contrast-stretch 0.55 -gravity Center -pointsize 50 -fill yellow -draw 'text 0,0 "Dieser PC wird gerade benutzt!"' /tmp/.screen_locked.png
+#  
+#  # Lock screen displaying this image.
+#  i3lock -i /tmp/.screen_locked.png
 
 # Turn the screen off after a delay.
 for i in $(seq 1 20)
